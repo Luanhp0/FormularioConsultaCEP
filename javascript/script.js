b19465d451af1c2a2854621ee30acd491451cd85
Your Name <you@example.com>
2024-09-06 14:07:06 -0300

    ajustando o consulta cep

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,17 @@
 
 
-  function consultarCEP(cep) {
-    // Verifica se o CEP tem 8 dígitos e é composto apenas por números
-    if (cep.length === 8 && /^\d+$/.test(cep)) {
-      fetch(`https://viacep.com.br/ws/${cep}/json/`)
-        .then(response => {
-          // Verifica se a resposta foi bem-sucedida
-          if (!response.ok) {
-            throw new Error('Erro ao consultar o CEP');
-          }
-          return response.json();
-        })
-        .then(data => preencherCampos(data))
-        .catch(error => console.error('Erro:', error));
-    } else {
-      console.error('CEP inválido');
-    }
+function consultarCEP(cep) {
+  if (cep.length == 8) { // Verifica se o CEP tem 8 dígitos
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(response => response.json())
+      .then(data => preencherCampos(data))
+      .catch(error => console.error('Erro:', error));
   }
+}
+
+function preencherCampos(dados) {
+  document.getElementById('logradouro').value = dados.logradouro || '';
+  document.getElementById('bairro').value = dados.bairro || '';
+  document.getElementById('cidade').value = dados.localidade || '';
+  document.getElementById('estado').value = dados.uf || '';
+}
